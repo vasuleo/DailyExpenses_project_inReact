3c0729d3170216734c99474e19c721d138be950b
vasuleo <vasudevan1606@gmail.com>
2022-03-23 16:36:27 +0530

    added keys

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Expence from "./componentes/Expense/Expence";
 import NewExpence from "./componentes/NewExpence/NewExpense";
 
+const DUMMYDATA = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expence, setExpence] = useState(DUMMYDATA);
 
   const addExpence = (expens) => {
-    console.log("App.js");
-    console.log(expens);
+    setExpence((prevsData) => [expens, ...prevsData]);
   };
 
   return (
     <div>
       <NewExpence onAddExpense={addExpence} />
-      <Expence item={expenses} />
+      <Expence item={expence} />
     </div>
   );
 }
--- src/componentes/Expense/Expence.js
+++ src/componentes/Expense/Expence.js
@@ -15,26 +15,14 @@ function Expenses(props) {
         selected={expenceFiler}
         onExpenceFilter={onChangeFilter}
       />
-      <ExpnceItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      />
-      <ExpnceItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpnceItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpnceItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
+      {props.item.map((expence) => (
+        <ExpnceItem
+          key={expence.id}
+          title={expence.title}
+          amount={expence.amount}
+          date={expence.date}
+        />
+      ))}
     </div>
   );
 }
